1b882ddd150a6a55cf51e5ceb8963b6524baa759
Kexin-Zhang-UCAS <1033477342@qq.com>
2021-09-07 11:38:55 -0400

    Add setup.py and MANIFEST.in.

--- Label.py
+++
@@ -1,9 +0,0 @@
-from type import *
-
-class Label(dict):
-    def __init__(self,tag:string,parameters:LabelSpec):
-        super().__init__()
-        keys = locals().keys() - ['__class__', "self"]
-        for key in keys:
-            if eval(key) != None:
-                self[key] = eval(key)
